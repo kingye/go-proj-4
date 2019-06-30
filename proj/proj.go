5c4487b2ab4b525f39a41fb20cc8e035cae3842c
Ye Jin <king.ye@me.com>
2019-06-30 17:07:50 +0800

    fix for proj 6.1.0

--- proj/proj.go
+++ proj/proj.go
@@ -28,7 +28,12 @@ package proj
 /*
 #cgo darwin pkg-config: proj
 #cgo !darwin LDFLAGS: -lproj
-#include "proj.h"
+#cgo CFLAGS: -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H
+#include <proj_api.h>
+char *transform(projPJ srcdefn, projPJ dstdefn, long point_count, double *x, double *y, double *z);
+char *fwd(projPJ src, double *lng, double *lat);
+char *inv(projPJ dst, double *lng, double *lat);
+char *get_err(void);
 */
 import "C"
 
